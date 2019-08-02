88546d01ce8155cfb64cf98eaf745607921cab64
rob Arseneault <shoelessrob@gmail.com>
2019-08-02 08:31:43 -0300

    upgrade pip and make sure mas is installed

--- osx-update-all.sh
+++ osx-update-all.sh
@@ -9,8 +9,10 @@ brew doctor
 brew missing
 #echo "you can hit mas upgrade to upgrade theses apps from the app store:"
 #mas outdated
+brew install mas
 mas upgrade
 npm update -g
+pip install --upgrade pip
 pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
 
